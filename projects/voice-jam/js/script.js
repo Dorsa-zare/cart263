bb67e4cf046decc8166ee585a17e38a48df4ac19
Dorsa-zare <D_zare@live.concordia.ca>
2024-02-03 10:56:26 -0500

    p1: Added basic structure and changed title

--- projects/voice-jam/js/script.js
+++ projects/voice-jam/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Voice Jam
+Dorsa Zare
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,31 @@ author, and this description to match your project!
 
 "use strict";
 
+const speechRecognizer = new p5.SpeechRec();
+let backgroundColor = `black`;
 
-/**
-Description of preload
-*/
 function preload() {
-
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+    createCanvas(550, 550);
 
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
+    speechRecognizer.start();
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+    background(backgroundColor);
+    // textAlign(CENTER, CENTER);
+    // textSize(30)
+    // text(`Say turn the lights lights on or off`, width / 2, height / 3);
+
+    // text(currentSpeech, width / 2, height / 2);
+}
+
+function handleSpeechInput() {
+    backgroundColor = speechRecognizer.resultString;
 
 }
