2e1f07b8e409f477f17728af82393f8f9e5665de
Dorsa-zare <D_zare@live.concordia.ca>
2024-02-03 13:59:28 -0500

    p1: Added states of the game.

--- projects/voice-jam/js/script.js
+++ projects/voice-jam/js/script.js
@@ -9,13 +9,13 @@ author, and this description to match your project!
 "use strict";
 
 const speechRecognizer = new p5.SpeechRec();
-let backgroundColor = `black`;
+let currentState = "Title"; // Initial state
 
 function preload() {
 }
 
 function setup() {
-    createCanvas(550, 550);
+    createCanvas(650, 550);
 
     speechRecognizer.onResult = handleSpeechInput;
     speechRecognizer.continuous = true;
@@ -24,12 +24,32 @@ function setup() {
 }
 
 function draw() {
-    background(backgroundColor);
-    // textAlign(CENTER, CENTER);
-    // textSize(30)
-    // text(`Say turn the lights lights on or off`, width / 2, height / 3);
+    background(150, 200, 150);
+    textAlign(CENTER, CENTER);
+    textSize(18);
 
-    // text(currentSpeech, width / 2, height / 2);
+    // Check the current state
+    if (currentState === "Title") {
+        displayTitle();
+    } else if (currentState === "Game") {
+        displayGame();
+    } else if (currentState === "Ending") {
+        displayEnding();
+    }
+}
+
+
+function displayTitle() {
+    text(`Help the cat find her food!\nRemember, cats don't like to follow human commands.\nUse the wrong direction commands to guide the cat in the right direction.`, width / 2, height / 2 - 110);
+
+    // Text for starting the game
+    textSize(16);
+    text("Say 'start' when you're ready!", width / 2, height / 2 + 50);
+
+    // Check if the user said 'start' to transition to the next state
+    if (currentSpeech.toLowerCase().includes("start")) {
+        currentState = "Game";
+    }
 }
 
 function handleSpeechInput() {
