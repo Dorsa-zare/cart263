c3ab6876324cf005494f4d2eaee49af304ba1498
Dorsa-zare <D_zare@live.concordia.ca>
2024-02-03 09:00:35 -0500

    Ex: voice and speech rec

--- experiments/p5.speech-experiments/js/script.js
+++ experiments/p5.speech-experiments/js/script.js
@@ -7,8 +7,8 @@ author, and this description to match your project!
 */
 
 "use strict";
-
-
+// let voice = new p5.Speech();
+let recognizer = new p5.SpeechRec();
 /**
 Description of preload
 */
@@ -21,7 +21,8 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+    recognizer.onResult = handleResult;
+    recognizer.start();
 }
 
 
@@ -30,4 +31,12 @@ Description of draw()
 */
 function draw() {
 
+}
+
+// function mousePressed() {
+//     voice.speak(`Dorsa, I really think you're doing a great job`)
+// }
+
+function handleResult() {
+    console.log(recognizer.resultString);
 }
